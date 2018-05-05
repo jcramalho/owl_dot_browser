e5b58158649c2b69a258f23da36c05a195384069
José Carlos Ramalho <jcramalho@users.noreply.github.com>
2018-05-05 16:08:45 +0100

    Agora, sem id arranca com o ponto inicial da ontologia

--- routes/sparql3.js
+++ routes/sparql3.js
@@ -3,8 +3,8 @@ var router = express.Router()
 
 const SparqlClient = require('sparql-client-2');
 const SPARQL = SparqlClient.SPARQL;
-const endpoint = 'http://epl.di.uminho.pt:40003/repositories/M51-CLAV'
-const myupdateEndpoint = 'http://epl.di.uminho.pt:40003/repositories/M51-CLAV/statements'
+const endpoint = 'http://localhost:7200/repositories/clav20180505'
+const myupdateEndpoint = 'http://localhost:7200/repositories/clav20180505/statements'
 
 var client = new SparqlClient( endpoint, {updateEndpoint: myupdateEndpoint, 
                                           defaultParameters: {format: 'json'}})
@@ -47,6 +47,55 @@ router.get('/:id', (req, res,next)=>{
                     'rankdir=LR ;\n' +
                     'node [style="filled"];\n' +
                     'start [label="' + id + '",fillcolor=lightgrey];\n'
+
+            for(j in myData){
+                for(k in myData[j]){
+                    var l = myData[j][k].slice(0,40)
+                    dot += 'f'+j+k+' [label="'+l+'",href="https://owl-dot-browser.herokuapp.com/'+l+'"];\n'
+                    dot += 'start -> f'+j+k+' [label="'+j+'"];\n'
+                }
+            }
+
+            dot += '}'
+            console.log('\n\n\n'+dot)
+            // res.send('<p>OK</p>')
+            res.render("showClasse", {renderingCode:'d3.select("#graph").graphviz().renderDot(\`'+dot+'\`)'})
+        })
+        .catch(function (error) {
+            console.log('ERRO: ' + error)
+            res.render("error", {error: error})
+        })
+})
+
+// Se não for passado o id, mostro o ponto de entrada principal
+router.get('/', (req, res,next)=>{
+    var dot = ""
+    /* Vou buscar numa query a informação toda */
+    var query = "select * where {\n" +
+        "clav:lc1 ?p ?o .\n" +
+        "}"
+
+    client.query(query)
+        .execute()
+        .then(function (qres) {
+            console.log(JSON.stringify(qres))
+            var resList = qres.results.bindings
+            var myData = {}
+            var p = ""
+            var o = ""
+            for(var i in resList){
+                p = resList[i].p.value.slice(resList[i].p.value.indexOf('#')+1)
+                o = resList[i].o.value.slice(resList[i].o.value.indexOf('#')+1)
+                if(myData[p])
+                  myData[p].push(o)
+                else
+                  myData[p] = [o]
+            }
+            console.log('MYDATAJSON: \n\n'+JSON.stringify(myData))
+            var dot = " digraph Diagrama {\n" +
+                    'rankdir=LR ;\n' +
+                    'node [style="filled"];\n' +
+                    'start [label="Lista Consolidada",fillcolor=lightgrey];\n'
 
             for(j in myData){
                 for(k in myData[j]){
